d3c216747b04bcf139a277a8dff74353d7a244e5
Sushil Anand <sushil.anand@gdn-commerce.com>
2018-06-22 12:20:15 +0530

    first commit

---
+++ src/main/java/com/coviam/codiecon/paperless/PaperlessApplication.java
@@ -0,0 +1,12 @@
+package com.coviam.codiecon.paperless;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaperlessApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaperlessApplication.class, args);
+	}
+}
